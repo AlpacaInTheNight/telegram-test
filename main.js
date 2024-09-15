889875548b9d8eea0396897b6490408421de881a
AlpacaInTheNight <greenwoods11a@gmail.com>
2024-09-15 04:07:14 +0300

    test

--- main.js
+++ main.js
@@ -3,12 +3,12 @@ window.Telegram.WebApp.expand();
 
 console.log(window.Telegram.WebApp);
 
-//document.writeln(JSON.stringify(window.Telegram.WebApp.user, null, "<br />"));
-//document.writeln(JSON.stringify(window.Telegram.WebApp.user, null, "<br />"));
+document.writeln(JSON.stringify(window.Telegram.WebApp.user, null, "<br />"));
+document.writeln(JSON.stringify(window.Telegram.WebApp.initDataUnsafe.user, null, "<br />"));
 
-document.writeln(
+/* document.writeln(
 	JSON.stringify(window.Telegram, null, "<br />")
-);
+); */
 
 /* document.writeln(
 	JSON.stringify(window.Telegram.WebApp, null, "<br />")
