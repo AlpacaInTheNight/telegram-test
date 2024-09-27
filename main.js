2743bb78462247365c975d17d12c9de65ae739c1
AlpacaInTheNight <greenwoods11a@gmail.com>
2024-09-27 08:32:34 +0300

    test

--- main.js
+++ main.js
@@ -12,6 +12,12 @@ document.writeln("<hr />");
 document.writeln(JSON.stringify(window.Telegram.WebApp.colorScheme, null, "<br />"));
 document.writeln("<hr />");
 
+document.writeln(JSON.stringify(window.Telegram.WebApp.initDataUnsafe.user.is_bot, null, "<br />"));
+document.writeln("<hr />");
+
+document.writeln(JSON.stringify(window.Telegram.WebApp.initDataUnsafe.query_id, null, "<br />"));
+document.writeln("<hr />");
+
 if(window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
 	alert("Is dark mode");
 }
